5b009eadddc05437f776f4e9007b3fae2ce26d5d
Antoine Deloffre <antoine.deloffre@kelio.com>
2024-05-21 13:49:10 +0200

    css

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,19 +2,12 @@
 </script>
 
 <template>
-  <div class="m-auto">
+  <div class="flex h-screen flex-col items-center justify-center text-center">
     <Navigation/>
-    <div class="main-content">
-      <slot/>
-    </div>
-    <UNotifications />
+    <slot/>
+    <UNotifications/>
   </div>
 </template>
 
 <style scoped>
-.main-content {
-  display: flex;
-  justify-content: center;
-  margin: 20px;
-}
 </style>
