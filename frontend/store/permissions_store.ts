bcc7961ea2f6ebfb718bc413f61b52f54707be7a
De70x <antoine.deloffre7@gmail.com>
2024-06-28 05:40:46 +0200

    handle missing permissions

--- frontend/store/permissions_store.ts
+++ frontend/store/permissions_store.ts
@@ -1,20 +1,22 @@
 export const usePermissionsStore = defineStore('permissions', {
     state: () => ({
-        permissions: [] as String[],
+        permissions: [] as String[] | undefined,
     }),
     actions: {
-        async fetchPermissions() {
+        async fetchPermissions(): Promise<String[]> {
             const {$api} = useNuxtApp()
             try {
                 const response = await $api.get('/user/permissions');
                 if (response) {
                     this.permissions = response.data
+                    return response.data
                 }
             } catch (error) {
                 console.error('Failed to fetch permissions:', error);
             }
+            return []
         },
-        clearPermissions(){
+        clearPermissions() {
             this.permissions = []
         }
     }
