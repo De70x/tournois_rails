a0fc4b045ae8859312cd59d7b48d9709efbefcdc
De70x <antoine.deloffre7@gmail.com>
2024-08-31 08:09:02 +0200

    permissions store with useState

--- frontend/store/permissions_store.ts
+++ frontend/store/permissions_store.ts
@@ -1,17 +1,20 @@
-export const usePermissionsStore = defineStore('permissions', {
-    state: () => ({
-        permissions: [] as String[] | undefined,
-    }),
-    actions: {
-        async fetchPermissions(): Promise<void> {
-            const {$api} = useNuxtApp()
-            const response = await $api.get<any>('/user/permissions');
-            if (response) {
-                this.permissions = response.data
-            }
-        },
-        clearPermissions() {
-            this.permissions = []
+export const usePermissionsStore = () => {
+    const permissions = useState<String[]>('permissions', () => [])
+    const fetchPermissions = async (): Promise<void> => {
+        const {$api} = useNuxtApp()
+        const response = await $api.get<any>('/user/permissions');
+        if (response) {
+            permissions.value = response.data
         }
     }
-});
+
+    const clearPermissions = () => {
+        permissions.value = []
+    }
+
+    return {
+        permissions,
+        fetchPermissions,
+        clearPermissions
+    }
+}
