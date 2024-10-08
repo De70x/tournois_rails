47888e3241e0f894f96d0d0b2568dd5d97a6a22a
De70x <antoine.deloffre7@gmail.com>
2024-10-08 06:12:04 +0200

    review match constraint

--- backend/app/models/matchs_tournoi.rb
+++ backend/app/models/matchs_tournoi.rb
@@ -6,5 +6,20 @@ class MatchsTournoi < ApplicationRecord
   belongs_to :stade, optional: true
   belongs_to :tableau_final, optional: true
 
-  validates_uniqueness_of :joueur1_id, scope: %i[joueur2_id phase]
+  validate :match_unique_pour_les_poules
+
+  private
+
+  def match_unique_pour_les_poules
+    if phase == -1
+      existing_match = MatchsTournoi.where(joueur1_id: joueur1_id, joueur2_id: joueur2_id, phase: -1)
+                                    .or(MatchsTournoi.where(joueur1_id: joueur2_id, joueur2_id: joueur1_id, phase: -1))
+                                    .where.not(id: id)
+                                    .exists?
+      if existing_match
+        errors.add(:base, "Un match existe déjà pour ces joueurs dans la phase -1")
+      end
+    end
+  end
+
 end
