40960fff75619751f990b84b9fd0e1dcea071eb2
De70x <antoine.deloffre7@gmail.com>
2024-10-20 13:55:58 +0200

    render files

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001, https://tournois-tcpm.onrender.com' # Remplacez '*' par les origines autorisées si nécessaire
+    origins ENV['FRONTEND_URL'] || 'localhost:3001'
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options],
